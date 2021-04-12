6188eecd5d23afccb06b381db2a818426db71bfe
ScreamerUWU <64812688+ScreamerUWU@users.noreply.github.com>
2021-04-12 02:20:20 +0100

    Update Main.lua

--- Scripts/ItemDropGUI/Main.lua
+++ Scripts/ItemDropGUI/Main.lua
@@ -20,239 +20,222 @@ SCRIPT
 ]]
 
 local Items = {
-"Rokakaka",
-"Mysterious Arrow",
-"Pure Rokakaka",
-"DEO's Diary",
-"Rib Cage of The Saint's Corpse",
-"Steel Ball",
-"Quinton's Glove",
-"Zepellin's Headband",
-"Ancient Scroll",
-"Stone Mask",
-"Diamond",
-"Gold Coin",
-"Lucky Arrow",
-"Left Arm Of The Saint's Corpse"
+    "Rokakaka", "Mysterious Arrow", "Pure Rokakaka", "DEO's Diary",
+    "Rib Cage of The Saint's Corpse", "Steel Ball", "Quinton's Glove",
+    "Zepellin's Headband", "Ancient Scroll", "Stone Mask", "Diamond",
+    "Gold Coin", "Lucky Arrow", "Left Arm Of The Saint's Corpse"
 }
 
 local function AttemptToDrop(Name, Time)
-   
-   local Amount = 0
-   
-   if DropAmount == "" or tonumber(DropAmount) == tonumber(0) then
-   
-   for Index, Obj in pairs(game:GetService("Players").LocalPlayer.Backpack:GetChildren()) do
-      if Obj.Name == (Name) then
-        game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer("DropItem", Obj)
-        wait(Time)
-      end
-   end
-   
-   end
-   
-   if DropAmount ~= "" and tonumber(DropAmount) ~= 0 then
-      
-      
-      for Index, Obj in pairs(game:GetService("Players").LocalPlayer.Backpack:GetChildren()) do
-         if Obj.Name == (Name) then
-             
-            if Amount == tonumber(DropAmount) then return end
-             
-            Amount = Amount + 1
-            
-            game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer("DropItem", Obj)
-            wait(Time)
-         end
-      end
-      
-   end
+
+    local Amount = 0
+
+    if DropAmount == "" or tonumber(DropAmount) == tonumber(0) then
+
+        for Index, Obj in pairs(
+                              game:GetService("Players").LocalPlayer.Backpack:GetChildren()) do
+            if Obj.Name == (Name) then
+                game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(
+                    "DropItem", Obj)
+                wait(Time)
+            end
+        end
+
+    end
+
+    if DropAmount ~= "" and tonumber(DropAmount) ~= 0 then
+
+        for Index, Obj in pairs(
+                              game:GetService("Players").LocalPlayer.Backpack:GetChildren()) do
+            if Obj.Name == (Name) then
+
+                if Amount == tonumber(DropAmount) then return end
+
+                Amount = Amount + 1
+
+                game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(
+                    "DropItem", Obj)
+                wait(Time)
+            end
+        end
+
+    end
 end
 
 local function SellItem(Name, Wait)
-    
+
     local Info = {
-     "EndDialogue",
-     {
-      NPC = "Merchant",
-      Option = "Option1",
-      Dialogue = "Dialogue5"
-     }
+        "EndDialogue",
+        {NPC = "Merchant", Option = "Option1", Dialogue = "Dialogue5"}
     }
-    
-       
-   local Amount = 0
-   
-   if SellAmount == "" or tonumber(SellAmount) == tonumber(0) then
-   
-   for Index, Obj in pairs(game:GetService("Players").LocalPlayer.Backpack:GetChildren()) do
-      if Obj.Name == (Name) then
-        Obj.Parent = game:GetService("Players").LocalPlayer.Character
-            
-        game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(Info))
-        wait(Time)
-      end
-   end
-   
-   end
-   
-   if SellAmount ~= "" and tonumber(SellAmount) ~= 0 then
-      
-      
-      for Index, Obj in pairs(game:GetService("Players").LocalPlayer.Backpack:GetChildren()) do
-         if Obj.Name == (Name) then
-             
-            if Amount == tonumber(SellAmount) then return end
-             
-            Amount = Amount + 1
-            
-            Obj.Parent = game:GetService("Players").LocalPlayer.Character
-            
-            game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(Info))
-            wait(Time)
-         end
-      end
-      
-   end
+
+    local Amount = 0
+
+    if SellAmount == "" or tonumber(SellAmount) == tonumber(0) then
+
+        for Index, Obj in pairs(
+                              game:GetService("Players").LocalPlayer.Backpack:GetChildren()) do
+            if Obj.Name == (Name) then
+                Obj.Parent = game:GetService("Players").LocalPlayer.Character
+
+                game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(
+                    unpack(Info))
+                wait(Time)
+            end
+        end
+
+    end
+
+    if SellAmount ~= "" and tonumber(SellAmount) ~= 0 then
+
+        for Index, Obj in pairs(
+                              game:GetService("Players").LocalPlayer.Backpack:GetChildren()) do
+            if Obj.Name == (Name) then
+
+                if Amount == tonumber(SellAmount) then return end
+
+                Amount = Amount + 1
+
+                Obj.Parent = game:GetService("Players").LocalPlayer.Character
+
+                game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(
+                    unpack(Info))
+                wait(Time)
+            end
+        end
+
+    end
 end
 
 local function RollArcade()
-    
-   local ArcadeInfo = {
-      "EndDialogue",
-      {
-       NPC = "Item Machine",
-       Option = "Option1",
-       Dialogue = "Dialogue1"
-      }
-   }
-   
-   game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(ArcadeInfo))
+
+    local ArcadeInfo = {
+        "EndDialogue",
+        {NPC = "Item Machine", Option = "Option1", Dialogue = "Dialogue1"}
+    }
+
+    game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(
+        unpack(ArcadeInfo))
 end
- 
+
 UIWINDOW:AddBox({
-                    text = "Drop Amount",
-                    value = DropAmount,
-                    callback = function(Amount)
-                       DropAmount = Amount
-                       warn(DropAmount)
-                    end
+    text = "Drop Amount",
+    value = DropAmount,
+    callback = function(Amount)
+        DropAmount = Amount
+        warn(DropAmount)
+    end
 })
 
 UIWINDOW:AddSlider({
-                    text = "Drop Time",
-                    min = 1.1,
-                    max = 3,
-                    float = 0,
-                    value = ItemDropWait,
-                    callback = function(Amount)
-                       ItemDropWait = Amount
-                    end
+    text = "Drop Time",
+    min = 1.1,
+    max = 3,
+    float = 0,
+    value = ItemDropWait,
+    callback = function(Amount) ItemDropWait = Amount end
 })
 
 for Index, TableValue in pairs(Items) do
-   
-   local IsRib = false
-   local IsArm = false
-   
-   if TableValue == ("Rib Cage of The Saint's Corpse") then
-      UIWINDOW:AddButton({
-                       text = "Rib Cage",
-                       callback = function()
-                          AttemptToDrop(TableValue, ItemDropWait)
-                       end
-       }) 
-       IsRib = true
-   elseif TableValue == ("Left Arm Of The Saint's Corpse") then
-      UIWINDOW:AddButton({
-                       text = "Left Arm",
-                       callback = function()
-                          AttemptToDrop(TableValue, ItemDropWait)
-                       end
-      })  
-      IsArm = true
-   end
-   
-   if not IsArm and not IsRib then
-      UIWINDOW:AddButton({
-                       text = TableValue,
-                       callback = function()
-                          AttemptToDrop(TableValue, ItemDropWait)
-                       end
-   }) 
-   end
+
+    local IsRib = false
+    local IsArm = false
+
+    if TableValue == ("Rib Cage of The Saint's Corpse") then
+        UIWINDOW:AddButton({
+            text = "Rib Cage",
+            callback = function()
+                AttemptToDrop(TableValue, ItemDropWait)
+            end
+        })
+        IsRib = true
+    elseif TableValue == ("Left Arm Of The Saint's Corpse") then
+        UIWINDOW:AddButton({
+            text = "Left Arm",
+            callback = function()
+                AttemptToDrop(TableValue, ItemDropWait)
+            end
+        })
+        IsArm = true
+    end
+
+    if not IsArm and not IsRib then
+        UIWINDOW:AddButton({
+            text = TableValue,
+            callback = function()
+                AttemptToDrop(TableValue, ItemDropWait)
+            end
+        })
+    end
 end
 
 UIWINDOWS:AddBox({
-                    text = "Sell Amount",
-                    value = SellAmount,
-                    callback = function(Amount)
-                       SellAmount = Amount
-                       warn(SellAmount)
-                    end
+    text = "Sell Amount",
+    value = SellAmount,
+    callback = function(Amount)
+        SellAmount = Amount
+        warn(SellAmount)
+    end
 })
 
 for Index, TableValue in pairs(Items) do
-   
-   local IsRib = false
-   local IsArm = false
-   
-   if TableValue == ("Rib Cage of The Saint's Corpse") then
-      UIWINDOWS:AddButton({
-                       text = "Rib Cage",
-                       callback = function()
-                          SellItem(TableValue, 0)
-                       end
-       }) 
-       IsRib = true
-   elseif TableValue == ("Left Arm Of The Saint's Corpse") then
-      UIWINDOWS:AddButton({
-                       text = "Left Arm",
-                       callback = function()
-                          SellItem(TableValue, 0)
-                       end
-      })  
-      IsArm = true
-   end
-   
-   if not IsArm and not IsRib then
-      UIWINDOWS:AddButton({
-                       text = TableValue,
-                       callback = function()
-                          SellItem(TableValue, 0)
-                       end
-   }) 
-   end
+
+    local IsRib = false
+    local IsArm = false
+
+    if TableValue == ("Rib Cage of The Saint's Corpse") then
+        UIWINDOWS:AddButton({
+            text = "Rib Cage",
+            callback = function() SellItem(TableValue, 0) end
+        })
+        IsRib = true
+    elseif TableValue == ("Left Arm Of The Saint's Corpse") then
+        UIWINDOWS:AddButton({
+            text = "Left Arm",
+            callback = function() SellItem(TableValue, 0) end
+        })
+        IsArm = true
+    end
+
+    if not IsArm and not IsRib then
+        UIWINDOWS:AddButton({
+            text = TableValue,
+            callback = function() SellItem(TableValue, 0) end
+        })
+    end
 end
 
 UIWINDOWM:AddToggle({
-  text = "Auto Arcade",
-  callback = function(Bool)
-      AA = true
-  end
+    text = "Auto Arcade",
+    callback = function(Bool) AA = true end
 })
 
 UIWINDOWM:AddToggle({
-  text = "Admin AutoKick",
-  callback = function(Bool)
-      IFAJ = true
-  end
+    text = "Admin AutoKick",
+    callback = function(Bool) IFAJ = true end
 })
 
 UILIBRARY:Init()
 
-game:GetService("Players").PlayerAdded:Connect(function(Player)
-    if Player:IsInGroup(3194064) and IFAJ then game:GetService("Players").LocalPlayer:Kick("Admin Joined!") end   
-end)
-   
-game:GetService("RunService").RenderStepped:Connect(function(...)
-    if AA then
-       RollArcade()  
-    end
-end)
+game:GetService("Players").PlayerAdded:Connect(
+    function(Player)
+        if Player:IsInGroup(3194064) and IFAJ then
+            game:GetService("Players").LocalPlayer:Kick("Admin Joined!")
+        end
+    end)
+
+game:GetService("RunService").RenderStepped:Connect(
+    function(...) if AA then RollArcade() end end)
 
 game.StarterGui:SetCore("SendNotification", {
-  Title = "Script;",
-  Text = "Successfully Loaded! Made by Screamer#2252",
-  Duration = 6
+    Title = "Script;",
+    Text = "Successfully Loaded! Made by Screamer#2252",
+    Duration = 6
 })
+
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Script Info;",
+    Text = "Code made neater by Chrrxs ( God )",
+    Duration = 3
+})
+
