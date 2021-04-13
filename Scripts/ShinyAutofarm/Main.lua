1f47f4c8d0f52b8eecddb60a89de82165ce9a433
ScreamerUWU <64812688+ScreamerUWU@users.noreply.github.com>
2021-04-13 03:27:05 +0100

    Create Main.lua

---
+++ Scripts/ShinyAutofarm/Main.lua
@@ -0,0 +1,142 @@
+local UI = game:HttpGet("https://pastebin.com/raw/edJT9EGX")
+local UILIBRARY = loadstring(UI)()
+local UIWINDOW = UILIBRARY:CreateWindow("Auto Farm")
+
+local ShinyFarm = false
+
+local function UseArrow()
+    
+    local ArrowInfo = {
+     "EndDialogue",
+     {NPC = "Mysterious Arrow", Option = "Option1", Dialogue = "Dialogue2"}
+    }
+    
+    if game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Mysterious Arrow") and not game:GetService("Players").LocalPlayer.Character:FindFirstChild("Mysterious Arrow") then
+    
+        local Arrow = game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Mysterious Arrow")
+        
+        Arrow.Parent = game:GetService("Players").LocalPlayer.Character
+        
+        game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(ArrowInfo))
+        
+    elseif not game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Mysterious Arrow") and game:GetService("Players").LocalPlayer.Character:FindFirstChild("Mysterious Arrow") then
+       
+        game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(ArrowInfo))
+    elseif not game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Mysterious Arrow") and not game:GetService("Players").LocalPlayer.Character:FindFirstChild("Mysterious Arrow") then
+       game.StarterGui:SetCore("SendNotification", {
+          Title = "Arrow;",
+          Text = "Wasn't Found!",
+          Duration = 3
+       })
+    end
+    
+end
+
+local function UseRoka()
+    
+    local RokaInfo = {
+     "EndDialogue",
+     {NPC = "Rokakaka", Option = "Option1", Dialogue = "Dialogue2"}
+    }
+    
+    if game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Rokakaka") and not game:GetService("Players").LocalPlayer.Character:FindFirstChild("Rokakaka") then
+    
+        local Arrow = game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Rokakaka")
+        
+        Arrow.Parent = game:GetService("Players").LocalPlayer.Character
+        
+        game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(RokaInfo))
+        
+    elseif not game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Rokakaka") and game:GetService("Players").LocalPlayer.Character:FindFirstChild("Rokakaka") then
+       
+        game:GetService("Players").LocalPlayer.Character.RemoteEvent:FireServer(unpack(RokaInfo))
+    elseif not game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Rokakaka") and not game:GetService("Players").LocalPlayer.Character:FindFirstChild("Rokakaka") then
+       game.StarterGui:SetCore("SendNotification", {
+          Title = "Rokakaka;",
+          Text = "Wasn't Found!",
+          Duration = 3
+       })
+    end
+    
+end
+
+local function MakeSkillTable(Name)
+    return {Skill = Name, SkillTreeType = "Character"}
+end
+
+local function SetWorthII()
+   local SkillsToDO = {
+        [1] = "Agility I", 
+        [2] = "Agility II", 
+        [3] = "Agility III", 
+        [4] = "Worthiness I", 
+        [5] = "Worthiness II"
+   }
+    
+   local RemoteFunction = game:GetService("Players").LocalPlayer.Character:WaitForChild("RemoteFunction")
+    
+   for i = 1, #SkillsToDO do
+      RemoteFunction:InvokeServer("LearnSkill", MakeSkillTable(SkillsToDO[i]))
+   end
+end
+
+local function StandWarn(StandNAME, Pity)
+    
+   local Shiny = ""
+   local Bind = Instance.new("BindableFunction")
+   
+   Bind.OnInvoke = function(Option)
+      if Option == ("Keep") then SF = false end
+      if Option == ("Re-Roll") then UseRoka() end
+   end
+   
+   if not Pity == (0) then return end
+    
+   game.StarterGui:SetCore("SendNotification", {
+      Title = "Shiny Rolled;",
+      Text = ("You Rolled a SHINY: " .. StandNAME),
+      Duration = 15,
+      Callback = Bind
+   })
+end
+
+local function ShinyCheck()
+   if game:GetService("Players").LocalPlayer:WaitForChild("PlayerStats"):WaitForChild("Stand").Value == ("None") then
+      SetWorthII()
+      UseArrow()
+        
+      repeat 
+            wait()
+      until game:GetService("Players").LocalPlayer:WaitForChild("PlayerStats"):WaitForChild("Stand").Value ~= ("None")
+        
+      StandWarn(game:GetService("Players").LocalPlayer:WaitForChild("PlayerStats"):WaitForChild("Stand").Value, game:GetService("Players").LocalPlayer:WaitForChild("PityCount").Value)
+   elseif game:GetService("Players").LocalPlayer:WaitForChild("PlayerStats"):WaitForChild("Stand").Value ~= ("None") then
+      if game:GetService("Players").LocalPlayer:FindFirstChild("PityCount") then
+          if not game:GetService("Players").LocalPlayer:WaitForChild("PityCount").Value == (0) or game:GetService("Players").LocalPlayer:WaitForChild("PityCount").Value ~= 0 then
+             UseRoka()
+          end
+      else
+         UseRoka()
+      end
+   end
+end
+
+UIWINDOW:CreateToggle({
+   text = ("Shiny Farm")
+   callback = function(Bool)
+      ShinyFarm = Bool
+      
+      if Bool then
+         ShinyCheck()
+      end
+   end
+})
+
+game:GetService("Players").LocalPlayer.CharacterAdded:Connect(function(Char)
+    if ShinyFarm then
+      wait(.2)
+      ShinyCheck()
+    end
+end)
+
+UILIBRARY:Init()
